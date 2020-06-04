1bf36c03e96271e9ed42ef64b1a24da0424b43c6
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-06-04 18:14:28 -0500

    test data-grid

--- src/app/data-grid-demo/data-grid-demo.module.ts
+++ src/app/data-grid-demo/data-grid-demo.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DataGridDemoRoutingModule } from './data-grid-demo-routing.module';
 import { DataGridDemoHomeComponent } from './data-grid-demo-home/data-grid-demo-home.component';
+import { DataGridModule } from '@poweredsoft/ngx-cdk-ui';
 
 
 @NgModule({
   declarations: [ DataGridDemoHomeComponent],
   imports: [
     CommonModule,
-    DataGridDemoRoutingModule
+    DataGridDemoRoutingModule,
+    DataGridModule
   ]
 })
 export class DataGridDemoModule { }
