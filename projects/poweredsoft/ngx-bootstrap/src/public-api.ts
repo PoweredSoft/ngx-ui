5bd9ae4d3b2dabd783d27e38ce50d1bb77ea55ab
Yubing325 <35515298+Yubing325@users.noreply.github.com>
2020-06-09 11:57:54 -0500

    modal init

---
+++ projects/poweredsoft/ngx-bootstrap/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-bootstrap
+ */
+
+export * from './lib/ngx-bootstrap.service';
+export * from './lib/ngx-bootstrap.component';
+export * from './lib/ngx-bootstrap.module';
