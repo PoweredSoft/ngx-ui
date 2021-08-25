af5cdbb4bd0b0aaf7e66fc3d5e99dd957e5ac6b2
David Lebee <david@poweredsoft.com>
2021-08-25 18:34:52 -0400

    v0.0.12

--- projects/poweredsoft/ngx-cdk-ui/src/lib/ds-validation-error/ds-validation-error.component.ts
+++ projects/poweredsoft/ngx-cdk-ui/src/lib/ds-validation-error/ds-validation-error.component.ts
@@ -12,15 +12,22 @@ export class DsValidationErrorComponent implements OnInit, OnDestroy {
   @Input() dataSource: IDataSource<any>;
   @Input() field: string;
   validationErrorsSub: Subscription;
+  commandStartedSub: Subscription;
   latestErrors: string[] = [];
 
   constructor() { }
 
   ngOnDestroy(): void {
     this.validationErrorsSub?.unsubscribe();
+    this.commandStartedSub?.unsubscribe();
   }
 
   ngOnInit(): void {
+    
+    this.commandStartedSub = this.dataSource.commandStarted$.subscribe(e => {
+      this.latestErrors = [];
+    });
+
     this.validationErrorsSub = this.dataSource.validationError$.subscribe(validationErrors => {
       this.latestErrors = Object.keys(validationErrors.errors)
         .filter(t => t.toLowerCase() == this.field?.toLowerCase())
